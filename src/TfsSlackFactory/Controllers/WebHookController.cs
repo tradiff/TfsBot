eb57f4981cc6d0fb1c571d9bbcce19bc2b5da53c
Lucas Falk <LRFalk01@gmail.com>
2016-05-12 22:25:13 -0400

    Real initial commit

---
+++ src/TfsSlackFactory/Controllers/WebHookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+
+namespace TfsSlackFactory.Controllers
+{
+    [Route("api/[controller]")]
+    public class WebHookController : Controller
+    {
+        // GET: api/values
+        [HttpPost("")]
+        public IActionResult Post()
+        {
+            StreamReader reader = new StreamReader(Request.Body);
+            var json = reader.ReadToEnd();
+            var obj = JsonConvert.DeserializeObject(json);
+            return Ok("Test");
+        }
+    }
+}
