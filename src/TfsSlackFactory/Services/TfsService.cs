d1087d783ff8f1546502ebfc426c6da828142ab6
Lucas Falk <LRFalk01@gmail.com>
2016-05-15 00:08:23 -0400

    run wiql against tfs api

--- src/TfsSlackFactory/Services/TfsService.cs
+++ src/TfsSlackFactory/Services/TfsService.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text;
 using Microsoft.Extensions.OptionsModel;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using TfsSlackFactory.Models;
 
@@ -19,12 +22,37 @@ namespace TfsSlackFactory.Services
 
         public void GetWorkItem(int workItemId)
         {
-            using (WebClient client = new WebClient())
+            using (WebClient client = GetWebClient())
             {
-                client.Credentials = _networkCredential;
                 var response = client.DownloadString($"{_baseAddress}_apis/wit/workItems/{workItemId}?$expand=relations&api-version=1.0");
                 dynamic responseObject = JObject.Parse(response);
             }
+        }
+
+        public bool IsWorkItemInQuery(int workItemId, string project, string query)
+        {
+            var requestUrl = $"{_baseAddress}{project}/_apis/wit/wiql?api-version=1.0";
+            using (WebClient client = GetWebClient())
+            {
+                client.Headers.Add("Content-Type", "application/json");
+                var jsonBody = JsonConvert.SerializeObject(new { query });
+                var response = client.UploadData(requestUrl, "POST", Encoding.UTF8.GetBytes(jsonBody));
+                dynamic responseObject = JObject.Parse(Encoding.UTF8.GetString(response));
+                
+                foreach (var workItem in responseObject.workItems)
+                {
+                    if (workItem.id == workItemId)
+                    {
+                        return true;
+                    }    
+                }
+                return false;
+            }
+        }
+
+        private WebClient GetWebClient()
+        {
+            return new WebClient {Credentials = _networkCredential};
         }
     }
 }
