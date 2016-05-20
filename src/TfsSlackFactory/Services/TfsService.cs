b7608224b3ca50f6eabbed024c19e60918b7a837
Lucas Falk <LRFalk01@gmail.com>
2016-05-19 21:18:57 -0400

    Switch TfsService to HttpClient

--- src/TfsSlackFactory/Services/TfsService.cs
+++ src/TfsSlackFactory/Services/TfsService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -12,49 +13,51 @@ namespace TfsSlackFactory.Services
 {
     public class TfsService
     {
-        private readonly NetworkCredential _networkCredential;
+        private readonly HttpClientHandler _clientHandler;
         private readonly string _baseAddress;
 
         public TfsService(IOptions<TfsSettings> tfsSettings)
         {
-            _networkCredential = new NetworkCredential(tfsSettings.Value.Username, tfsSettings.Value.Password);
+            _clientHandler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential(tfsSettings.Value.Username, tfsSettings.Value.Password)
+            };
+
             _baseAddress = tfsSettings.Value.Server;
         }
 
 
         public SlackWorkItemModel GetWorkItem(int workItemId)
         {
-            throw new NotImplementedException();
-            //using (WebClient client = GetWebClient())
-            //{
-            //    var response = client.DownloadString($"{_baseAddress}_apis/wit/workItems/{workItemId}?$expand=relations&api-version=1.0");
-            //    var obj = JsonConvert.DeserializeObject<TfsWorkItemModel>(response);
+            using (var client = GetWebClient())
+            {
+                var response = client.GetAsync($"{_baseAddress}_apis/wit/workItems/{workItemId}?$expand=relations&api-version=1.0").Result;
+                var obj = JsonConvert.DeserializeObject<TfsWorkItemModel>(response.Content.ReadAsStringAsync().Result);
 
-            //    var model = SlackWorkItemModel.FromTfs(obj);
+                var model = SlackWorkItemModel.FromTfs(obj);
 
-            //    if (obj.Relations != null && obj.Relations.Any(x => x.Rel == "System.LinkTypes.Hierarchy-Reverse"))
-            //    {
-            //        var parentModel = GetWorkItem(obj.Relations.Single(x => x.Rel == "System.LinkTypes.Hierarchy-Reverse").Url);
-            //        model.ParentWiId = parentModel.WiId;
-            //        model.ParentWiTitle = parentModel.WiTitle;
-            //        model.ParentWiType = parentModel.WiType;
-            //        model.ParentWiUrl = parentModel.WiUrl;
-            //    }
+                if (obj.Relations != null && obj.Relations.Any(x => x.Rel == "System.LinkTypes.Hierarchy-Reverse"))
+                {
+                    var parentModel = GetWorkItem(obj.Relations.Single(x => x.Rel == "System.LinkTypes.Hierarchy-Reverse").Url);
+                    model.ParentWiId = parentModel.WiId;
+                    model.ParentWiTitle = parentModel.WiTitle;
+                    model.ParentWiType = parentModel.WiType;
+                    model.ParentWiUrl = parentModel.WiUrl;
+                }
 
-            //    return model;
-            //}
+                return model;
+            }
         }
 
         private SlackWorkItemModel GetWorkItem(string workItemUrl)
         {
-            throw new NotImplementedException();
-            //using (WebClient client = GetWebClient())
-            //{
-            //    var response = client.DownloadString($"{workItemUrl}?$expand=relations&api-version=1.0");
-            //    var obj = JsonConvert.DeserializeObject<TfsWorkItemModel>(response);
+            using (var client = GetWebClient())
+            {
+                var response = client.GetAsync($"{workItemUrl}?$expand=relations&api-version=1.0").Result;
+                var obj = JsonConvert.DeserializeObject<TfsWorkItemModel>(response.Content.ReadAsStringAsync().Result);
 
-            //    return SlackWorkItemModel.FromTfs(obj);
-            //}
+                return SlackWorkItemModel.FromTfs(obj);
+            }
         }
 
         public bool IsWorkItemInQuery(int workItemId, string project, string query)
@@ -65,43 +68,44 @@ namespace TfsSlackFactory.Services
             }
 
             var requestUrl = $"{_baseAddress}{project}/_apis/wit/wiql?api-version=1.0";
-            throw new NotImplementedException();
-            //using (WebClient client = GetWebClient())
-            //{
-            //    client.Headers.Add("Content-Type", "application/json");
-            //    var jsonBody = JsonConvert.SerializeObject(new { query });
-            //    var response = client.UploadData(requestUrl, "POST", Encoding.UTF8.GetBytes(jsonBody));
-            //    dynamic responseObject = JObject.Parse(Encoding.UTF8.GetString(response));
+            using (var client = GetWebClient())
+            {
+                var jsonBody = JsonConvert.SerializeObject(new { query });
 
-            //    if (responseObject.workItemRelations != null)
-            //    {
-            //        foreach (var workItem in responseObject.workItemRelations)
-            //        {
-            //            if (workItem.source?.id == workItemId)
-            //            {
-            //                return true;
-            //            }
-            //        }
-            //        return false;
-            //    }
+                var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
 
-            //    if (responseObject.workItems != null)
-            //    {
-            //        foreach (var workItem in responseObject.workItems)
-            //        {
-            //            if (workItem.id == workItemId)
-            //            {
-            //                return true;
-            //            }
-            //        }
-            //    }
-            //    return false;
-            //}
+                var response = client.PostAsync(requestUrl, content).Result;
+                dynamic responseObject = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+
+                if (responseObject.workItemRelations != null)
+                {
+                    foreach (var workItem in responseObject.workItemRelations)
+                    {
+                        if (workItem.source?.id == workItemId)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                if (responseObject.workItems != null)
+                {
+                    foreach (var workItem in responseObject.workItems)
+                    {
+                        if (workItem.id == workItemId)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
         }
 
-        //private WebClient GetWebClient()
-        //{
-        //    return new WebClient {Credentials = _networkCredential};
-        //}
+        private HttpClient GetWebClient()
+        {
+            return new HttpClient(_clientHandler);
+        }
     }
 }
