203f0ef777a321241ed6c8fa08acf79de037ca36
Lucas Falk <LRFalk01@gmail.com>
2016-05-20 15:36:11 -0400

    Add Microsoft.CodeAnalysis.CSharp.Scripting. Rework Format

--- src/TfsSlackFactory/Services/FormatService.cs
+++ src/TfsSlackFactory/Services/FormatService.cs
@@ -1,4 +1,5 @@
-﻿using TfsSlackFactory.Models;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using TfsSlackFactory.Models;
 
 namespace TfsSlackFactory.Services
 {
@@ -6,28 +7,7 @@ namespace TfsSlackFactory.Services
     {
         public string Format(SlackWorkItemModel model, string formatString)
         {
-            string message = formatString;
-            message = message.Replace("{teamProjectCollection}", model.TeamProjectCollection);
-            message = message.Replace("{displayName}", model.DisplayName);
-            message = message.Replace("{projectName}", model.ProjectName);
-            message = message.Replace("{wiUrl}", model.WiUrl);
-            message = message.Replace("{wiType}", model.WiType);
-            message = message.Replace("{wiId}", model.WiId.ToString());
-            message = message.Replace("{wiTitle}", model.WiTitle);
-            message = message.Replace("{isStateChanged}", model.IsStateChanged.ToString());
-            message = message.Replace("{isAssigmentChanged}", model.IsAssigmentChanged.ToString());
-            message = message.Replace("{assignedTo}", model.AssignedTo);
-            message = message.Replace("{state}", model.State);
-            message = message.Replace("{userName}", model.UserName);
-            message = message.Replace("{action}", model.Action);
-            message = message.Replace("{assignedToUserName}", model.AssignedToUserName);
-            message = message.Replace("{mappedAssignedToUser}", model.MappedAssignedToUser);
-            message = message.Replace("{mappedUser}", model.MappedUser);
-            message = message.Replace("{parentWiUrl}", model.ParentWiUrl);
-            message = message.Replace("{parentWiType}", model.ParentWiType);
-            message = message.Replace("{parentWiId}", model.ParentWiId.ToString());
-            message = message.Replace("{parentWiTitle}", model.ParentWiTitle);
-            return message;
+            return CSharpScript.EvaluateAsync<string>("$@\"" + formatString + "\"", globals: model).Result;
         }
     }
 }
